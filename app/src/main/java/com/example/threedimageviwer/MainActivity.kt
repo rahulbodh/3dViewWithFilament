e0e00e93879915aea005966b7f53664a81948611
Rahul <rahulbodh819@gmail.com>
2024-08-02 17:00:47 +0530

    first commit

---
+++ app/src/main/java/com/example/threedimageviwer/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.threedimageviwer
+
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val mainView = findViewById<View>(R.id.simple_layout)
+        ViewCompat.setOnApplyWindowInsetsListener(mainView) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+    }
+
+}
